3a2c414751d6d958d6ac5e4bdfde700aa554c594
Илья Смаль <user925@realwebad.air>
2017-01-10 12:44:38 +0300

    suppliers page is ready

---
+++ __dev/___temp/scripts/suppliers-menu.js
@@ -0,0 +1,40 @@
+(function(){
+"use strict";
+App.run(function(){
+    App.onClick('.js-suppliers-menu-btn', function(e){
+        e.preventDefault();
+        var btn = $('.js-suppliers-menu-btn');
+        var menu = $('.js-suppliers-menu');
+        var btnIndex = btn.index(this);
+        var thisMenu = menu.eq(btnIndex);
+
+        if(!thisMenu.hasClass('__show')) {
+            thisMenu.addClass('__show');
+        } else {
+            thisMenu.removeClass('__show');
+        }
+    });
+
+    App.onClick('.js-suppliers-menu-close', function(e){
+        e.preventDefault();
+        var close = $(this);
+        var menu = close.closest('.js-suppliers-menu');
+
+        menu.removeClass('__show');
+    });
+
+    App.onClick('.js-suppliers-complex-btn', function(e){
+        e.preventDefault();
+        var btn = $(this);
+        var menu = btn.parent().find('.js-suppliers-complex-menu');
+
+        if(!menu.hasClass('__show')) {
+            btn.addClass('__clicked');
+            menu.addClass('__show');
+        } else {
+            btn.removeClass('__clicked');
+            menu.removeClass('__show');
+        }
+    });
+})
+})();
