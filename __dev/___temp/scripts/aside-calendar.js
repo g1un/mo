9c7a902e3385953b5d9207a0626b238c8fa292a3
Илья Смаль <user925@realwebad.air>
2017-01-12 13:35:41 +0300

    Profile page is ready

---
+++ __dev/___temp/scripts/aside-calendar.js
@@ -0,0 +1,26 @@
+(function(){
+"use strict";
+(function(){
+    "use strict";
+    App.gui.add({
+        _name: 'aside-calendar',
+        selector: '.js-calendar',
+        build: function($el){
+            var options = {
+                showOtherMonths: true
+            };
+
+            $el.datepicker(options);
+
+            return {
+                destroy: function(){
+                    $el.datepicker( "destroy" );
+                }
+            }
+        },
+        destroy: function($el, methods){
+            if(methods && typeof methods['destroy'] == 'function') methods['destroy']();
+        }
+    });
+})();
+})();
