ec30b3a300afdd9f2da37fe4e8bbf2962ee18243
Илья Смаль <user925@realwebad.air>
2017-01-25 12:24:00 +0300

    history page is ready

---
+++ __dev/___temp/scripts/history.js
@@ -0,0 +1,68 @@
+(function(){
+"use strict";
+App.run(function(){
+    // $(document).ready(function() {
+    //     // An array of dates
+    //     var eventDates = {};
+    //
+    //     eventDates[ new Date( '12/27/2016' )] = new Date( '12/27/2016' );
+    //     eventDates[ new Date( '12/28/2016' )] = new Date( '12/28/2016' );
+    //     eventDates[ new Date( '12/29/2016' )] = new Date( '12/29/2016' );
+    //     eventDates[ new Date( '01/01/2017' )] = new Date( '01/01/2017' );
+    //     eventDates[ new Date( '01/18/2017' )] = new Date( '01/18/2017' );
+    //     eventDates[ new Date( '01/19/2017' )] = new Date( '01/19/2017' );
+    //     eventDates[ new Date( '01/21/2017' )] = new Date( '01/21/2017' );
+    //
+    //     $('.js-calendar-markers').datepicker({
+    //         beforeShowDay: function(date) {
+    //             var Highlight = SelectedDates[date];
+    //             if (Highlight) {
+    //                 return [true, "Highlighted", Highlight];
+    //             }
+    //             else {
+    //                 return [true, '', ''];
+    //             }
+    //         }
+    //     });
+    // });
+
+    // App.onClick('.js-suppliers-menu-btn', function(e){
+    //     e.preventDefault();
+    //     var btn = $('.js-suppliers-menu-btn');
+    //     var menu = $('.js-suppliers-menu');
+    //     var btnIndex = btn.index(this);
+    //     var thisMenu = menu.eq(btnIndex);
+    //
+    //     if(!thisMenu.hasClass('__show')) {
+    //         thisMenu.addClass('__show');
+    //     } else {
+    //         thisMenu.removeClass('__show');
+    //     }
+    // });
+    //
+    // App.onClick('.js-suppliers-menu-close', function(e){
+    //     e.preventDefault();
+    //     var close = $(this);
+    //     var menu = close.closest('.js-suppliers-menu');
+    //
+    //     menu.removeClass('__show');
+    // });
+    //
+    // App.onClick('.js-suppliers-complex-btn', function(e){
+    //     e.preventDefault();
+    //     var clicked = $(e.target);
+    //     if(!clicked.closest('.js-input-volume').length) {
+    //         var btn = $(this);
+    //         var menu = btn.parent().find('.js-suppliers-complex-menu');
+    //
+    //         if(!menu.hasClass('__show')) {
+    //             btn.addClass('__clicked');
+    //             menu.addClass('__show');
+    //         } else {
+    //             btn.removeClass('__clicked');
+    //             menu.removeClass('__show');
+    //         }
+    //     }
+    // });
+});
+})();
---
+++ __dev/scripts/project/calendar.js
@@ -0,0 +1,53 @@
+(function(){
+    "use strict";
+    App.gui.add({
+        _name: 'aside',
+        selector: '.js-calendar',
+        build: function($el){
+            var options;
+
+            if($el.hasClass('js-calendar-markers')) {
+                // An array of dates
+                var completeDates = {};
+                var incompleteDates = {};
+                completeDates[ new Date( '12/27/2016' )] = new Date( '12/27/2016' ).toString();
+                completeDates[ new Date( '12/28/2016' )] = new Date( '12/28/2016' ).toString();
+                completeDates[ new Date( '12/29/2016' )] = new Date( '12/29/2016' ).toString();
+                completeDates[ new Date( '01/01/2017' )] = new Date( '01/01/2017' ).toString();
+                incompleteDates[ new Date( '01/18/2017' )] = new Date( '01/18/2017' ).toString();
+                incompleteDates[ new Date( '01/19/2017' )] = new Date( '01/19/2017' ).toString();
+                incompleteDates[ new Date( '01/21/2017' )] = new Date( '01/21/2017' ).toString();
+
+                options = {
+                    showOtherMonths: true,
+                    beforeShowDay: function(date) {
+                        var complete = completeDates[date];
+                        var incomplete = incompleteDates[date];
+                        if (complete) {
+                            return [true, "__complete", complete];
+                        } else if(incomplete) {
+                            return [true, "__incomplete", incomplete];
+                        } else {
+                            return [true, '', ''];
+                        }
+                    }
+                }
+            } else {
+                options = {
+                    showOtherMonths: true
+                };
+            }
+
+            $el.datepicker(options);
+
+            return {
+                destroy: function(){
+                    $el.datepicker( "destroy" );
+                }
+            }
+        },
+        destroy: function($el, methods){
+            if(methods && typeof methods['destroy'] == 'function') methods['destroy']();
+        }
+    });
+})();
