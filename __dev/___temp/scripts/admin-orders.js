ee1c7976b29cbe9dfc15cc4f365ed6f42e345061
Илья Смаль <user925@realwebad.air>
2017-02-06 15:44:12 +0300

    admin-orders page is almost ready

---
+++ __dev/___temp/scripts/admin-orders.js
@@ -0,0 +1,35 @@
+(function(){
+"use strict";
+App.run(function(){
+    $(document).ready(function() {
+        //select all function
+        var selectAllInput = $('[data-select-all]');
+        selectAllInput.each(function(idx) {
+            var _selectAllInput = $(this);
+            var selectAllItems = $('[data-select-all-item="' + _selectAllInput.attr('data-select-all') + '"]');
+            selectAllItems.change(function(id) {
+                if($(this).prop('checked') == false) {
+                    _selectAllInput.prop("checked", false);
+                    return;
+                } else {
+                    selectAllItems.each(function(el) {
+                        if($(this).prop('checked') == false) {
+                            _selectAllInput.prop("checked", false);
+                            return false;
+                        } else if(selectAllItems.length == (el+1)) {
+                            _selectAllInput.prop("checked", true);
+                        }
+                    });
+                }
+            });
+            $(selectAllInput[idx]).change(function() {
+                if(this.checked) {
+                    selectAllItems.prop("checked", true);
+                } else {
+                    selectAllItems.prop("checked", false);
+                }
+            });
+        });
+    });
+});
+})();
